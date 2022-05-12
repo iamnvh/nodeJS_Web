495d9d7c8b6e3f93723e53660dd8ee293d1a7afd
nguyenvanhao <nguyenvanhao.ktpm02@gmail.com>
2022-05-12 16:29:54 +0700

    complete

--- controllers/backend/admin.controller.js
+++ controllers/backend/admin.controller.js
@@ -4,23 +4,23 @@ const saltRounds = 10;
 const { mutipleMongooseToObject, mongooseToObject } = require('../../utli/convertDataToObject');
 
 class AdminController {
-  async index(req, res) {
-    const pageNumber = req.query.page;
-    const perPage = 4;
-    const [admins, totalAdmins] = await Promise.all([
+	async index(req, res) {
+		const pageNumber = req.query.page;
+		const perPage = 4;
+		const [admins, totalAdmins] = await Promise.all([
 			Admin_Model.find({}).limit(perPage).skip((pageNumber - 1) * perPage),
 			Admin_Model.countDocuments()
-    ]);
-    const pages = [];
-    const totalPages = Math.ceil(totalAdmins / perPage);
-    for (let i = 1; i <= totalPages; i++) {
-      pages.push(i)
-    }
-    return res.render('./backend/admin/list-admin', {
-      datas: mutipleMongooseToObject(admins),
-      pages: pages
-    });
-  }
+		]);
+		const pages = [];
+		const totalPages = Math.ceil(totalAdmins / perPage);
+		for (let i = 1; i <= totalPages; i++) {
+			pages.push(i)
+		}
+		return res.render('./backend/admin/list-admin', {
+			datas: mutipleMongooseToObject(admins),
+			pages: pages
+		});
+	}
 	async creat(req, res) {
 		try {
 			return res.render('./backend/admin/register')
@@ -28,13 +28,13 @@ class AdminController {
 			throw error;
 		}
 	}
-	async store(req,res) {
+	async store(req, res) {
 		try {
-			const admin = await Admin_Model.findOne({email: req.body.email});
-			if(admin) {
-				return res.render('./backend/admin/register', {erroremail: 'Email này đã được sử dụng.',email: req.body.email})
+			const admin = await Admin_Model.findOne({ email: req.body.email });
+			if (admin) {
+				return res.render('./backend/admin/register', { erroremail: 'Email này đã được sử dụng.', email: req.body.email })
 			} else {
-				bcrypt.hash(req.body.password, saltRounds).then(function(hash) {
+				bcrypt.hash(req.body.password, saltRounds).then(function (hash) {
 					Admin_Model.create({
 						name: req.body.username,
 						email: req.body.email,
@@ -49,6 +49,6 @@ class AdminController {
 		}
 	}
 }
-  
-    
+
+
 module.exports = new AdminController;

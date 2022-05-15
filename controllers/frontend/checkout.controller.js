a7b69c88621a71bcd106a284e213422c130f9193
nguyenvanhao <nguyenvanhao.ktpm02@gmail.com>
2022-05-15 08:23:04 +0700

    update send mail

--- controllers/frontend/checkout.controller.js
+++ controllers/frontend/checkout.controller.js
@@ -2,6 +2,8 @@ const Product_Model = require('../../models/book.model')
 const Cart_Model = require('../../models/cart.model');
 const Category_Model = require('../../models/category.model')
 const Order_Model = require('../../models/order.model')
+const User_Model = require('../../models/user.model')
+const nodemailer = require('nodemailer');
 const { verifyToken } = require('../../utli/verifyToken');
 const { mutipleMongooseToObject, mongooseToObject } = require('../../utli/convertDataToObject');
 
@@ -47,6 +49,42 @@ class CheckoutController {
           }
         }
         await Cart_Model.deleteOne({ user_id: decodedToken.id });
+        try {
+          const productOrder = await Order_Model.findOne({ user_id: decodedToken.id });
+          let name = "";
+          for(let i = 0; i <productOrder.products.length; i++) {
+            name += ""+productOrder.products[i].name+" - đơn giá: "+productOrder.products[i].price+"đ - số lượng : "+ productOrder.products[i].quantity+"<br>";
+          }
+          console.log(name)
+          const email = await User_Model.findOne({ _id: decodedToken.id})
+          let transporter = nodemailer.createTransport( {
+              host: 'smtp.gmail.com',
+              service: 'gmail',
+              auth: {
+                  user: process.env.EMAIL_AUTH,
+                  pass: process.env.EMAIL_PASSWORD,
+              }
+          });
+          await transporter.sendMail({
+              from:process.env.EMAIL_AUTH,
+              to: `${email.email}`,
+              subject: "Xin chào khách hàng",
+              html: "<h3>Xin chào</h3><p>Lời đầu tiên Dking xin cảm ơn quý khách hàng đã tin tưởng lựa chọn sản phẩm của chúng tôi.</p>" +
+                    "<p>Thông tin về đơn hàng của quý khách: </p>" + name +"Tổng giá trị đơn hàng: "+productOrder.totalPrice+ "đ <br>Địa chỉ nhận hàng: "+productOrder.address+
+                    "<br>Số điện thoại nhận hàng: "+ productOrder.phone+
+                    "<p>Vui lòng kiểm tra đơn hàng của quý khách.</p>" + "Có thắc mắc vui lòng liên hệ email: " +email.email
+                    
+          },(err) => {
+              if(err) {
+                  console.error(err);
+              }
+              else {
+                  console.log("Gửi mail thành công")
+              }
+          })
+        } catch (error) {
+          console.log(error)
+        }
         return res.redirect('/')
       } else {
         return res.redirect('/cart')
